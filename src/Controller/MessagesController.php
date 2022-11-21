33f8e36a29fbd84c84ba056c945a563d2997a067
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-11-21 18:48:16 +0100

    Add Success Flashes in messages

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessagesType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,4 +19,33 @@ class MessagesController extends AbstractController
             'controller_name' => 'MessagesController',
         ]);
     }
+
+    #[Route('/send', name: 'app_send')]
+   
+    public function send(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $message = new Message();
+        $form = $this->createForm(MessagesType::class, $message);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $message->setSender($this->getUser());
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            $this->addFlash("success", "Message successfully sent");
+            return $this->redirectToRoute("app_messages");
+        }
+
+        return $this->render("messages/send.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
 }
+
+
+    
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -172,4 +172,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->email;
+    }
 }
